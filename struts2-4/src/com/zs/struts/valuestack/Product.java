0c310df664d0d66dbb766733e328413233635255
happyte <1016917515@qq.com>
2017-06-15 22:44:02 +0800

    struts2值栈的对象栈和map栈，显示值的方式分别为value="object.shuxing"和value="#object.shuxing"

---
+++ struts2-4/src/com/zs/struts/valuestack/Product.java
@@ -0,0 +1,50 @@
+package com.zs.struts.valuestack;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.util.ValueStack;
+
+public class Product {
+	private Integer productId;
+	private String  productName;
+	private String productDesc;
+	private double productPrice;
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", productDesc=" + productDesc
+				+ ", productPrice=" + productPrice + "]";
+	}
+	public String save(){
+		ValueStack valueStack = ActionContext.getContext().getValueStack();
+		System.out.println("---------:"+ActionContext.getContext().getContextMap().get("request"));
+		Test test = new Test();
+		test.setProductDesc("blog");
+		test.setProductName("happyte");
+		test.setProductPrice(2000);
+		valueStack.push(test);
+		return "success";
+	}
+}
