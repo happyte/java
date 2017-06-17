7392ed89b85f41b1c2c721d0182e7d89b29fd93d
happyte <1016917515@qq.com>
2017-06-17 20:50:23 +0800

    继承ModelDriven接口，会返回一个Employee对象到值栈的栈顶，然后参数拦截器会给把参数给栈顶对象赋值,即EmpolyeeAction中的eployee对象

--- struts2-5/src/com/zs/struts/app/EmployeeAction.java
+++ struts2-5/src/com/zs/struts/app/EmployeeAction.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.RequestAware;
 
-public class EmployeeAction implements RequestAware {
+import com.opensymphony.xwork2.ModelDriven;
+
+public class EmployeeAction implements RequestAware, ModelDriven<Employee> {
 	
 	private Integer employeeId;
-	private String firstName;
-	private String lastName;
-	private String email;
+	private Employee employee;
 	private Dao dao = new Dao();
 	private Map<String, Object> requestMap = new HashMap<>();
 	
@@ -31,32 +31,18 @@ public class EmployeeAction implements RequestAware {
 		return employeeId;
 	}
 	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	public String delete(){
 		dao.delete(employeeId);
 		return "success";
 	}
 	
 	public String save(){
-		Employee employee = new Employee(null, firstName, lastName, email);
 		dao.save(employee);
 		return "success";
+	}
+	@Override
+	public Employee getModel() {
+		employee = new Employee();
+		return employee;
 	}
 }
