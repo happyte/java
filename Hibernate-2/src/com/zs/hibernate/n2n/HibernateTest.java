4d28f1d9103db0cd049dbaf32701e961ec29878f
happyte <1016917515@qq.com>
2017-06-26 21:28:34 +0800

    Hibernate 单向1-n

---
+++ Hibernate-2/src/com/zs/hibernate/n2n/HibernateTest.java
@@ -0,0 +1,65 @@
+package com.zs.hibernate.n2n;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HibernateTest {
+	
+	private SessionFactory sessionFactory;
+	private Session session;
+	private Transaction transaction;
+	
+	@Before
+	public void init(){
+		Configuration configuration = new Configuration().configure();
+		ServiceRegistry serviceRegistry = 
+				new ServiceRegistryBuilder().applySettings(configuration.getProperties())
+				                            .buildServiceRegistry();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+	}
+	
+	@After
+	public void destroy(){
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+	}
+	
+
+	@Test
+	public void testSave() {
+		Category category1 = new Category();
+		category1.setName("C-AA");
+		
+		Category category2 = new Category();
+		category2.setName("C-BB");
+		
+		Item item1 = new Item();
+		item1.setName("I-AA");
+		
+		Item item2 = new Item();
+		item2.setName("I-BB");
+		
+		category1.getItems().add(item1);
+		category1.getItems().add(item2);
+		category2.getItems().add(item1);
+		
+		session.save(category1);
+		session.save(category2);
+		session.save(item1);
+		session.save(item2);
+	}
+	
+	
+
+}
