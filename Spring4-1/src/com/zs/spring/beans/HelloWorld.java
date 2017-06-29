d3c15c32d27343409eebe71afb8590804523a3d2
happyte <1016917515@qq.com>
2017-06-29 21:48:38 +0800

    Spring环境搭建Helloworld,安装的时候竟然要把联网更新选上，而且我安装的是eclipse4.6.2版本

---
+++ Spring4-1/src/com/zs/spring/beans/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zs.spring.beans;
+
+public class HelloWorld {
+	private String name;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void hello(){
+		System.out.println("hello:"+name);
+	}
+}
