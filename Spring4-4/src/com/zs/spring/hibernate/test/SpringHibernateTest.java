2b7a9990af469253077bc25ed252ebb2e4c059a0
happyte <1016917515@qq.com>
2017-07-04 09:40:58 +0800

    Spring整合Hibernate准备工作

---
+++ Spring4-4/src/com/zs/spring/hibernate/test/SpringHibernateTest.java
@@ -0,0 +1,26 @@
+package com.zs.spring.hibernate.test;
+
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHibernateTest {
+	
+	private ApplicationContext ctx = null;
+	
+	{
+		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+
+	@Test
+	public void test() throws SQLException {
+		DataSource dataSource = ctx.getBean(DataSource.class);
+		System.out.println(dataSource.getConnection());
+	}
+
+}
