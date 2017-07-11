972a74cb3392a97cf0fcd2e92d1625f6bf821dd1
happyte <1016917515@qq.com>
2017-07-11 18:55:47 +0800

    测试springmvc restful的delete,put方法成功，忘记在web.xml中配置HiddenHttpMethodFilter了

--- SpringMVC-1/WebContent/index.jsp
+++ SpringMVC-1/WebContent/index.jsp
@@ -10,7 +10,47 @@
 	<form action="springmvc/testMethod" method="post">
 		<input type="submit" value="Submit">
 	</form>
-	<a href="hello">Hello</a><br>
+	<a href="hello">Hello</a>
+	<br>
+	
 	<a href="springmvc/testRequestMapping">testRequestMapping</a>
+	<br>
+	
+	<a href="springmvc/testMethod">testMethod</a>
+	<br>
+	
+	<form action="springmvc/testMethod" method="post">
+		<input type="submit" value="testMethod">
+	</form>
+	<br>
+	
+	<a href="springmvc/testHeadersAndParameters?username=zs&age=20">testHeadersAndParameters</a>
+	<br>
+	
+	<a href="springmvc/testAntPath/mnxyz/abc">Test AntPath</a>
+	<br>
+	
+	<a href="springmvc/testPathVariable/1">Test testPathVariable</a>
+	<br>
+	
+	<a href="springmvc/testRestGet/1">Test testGet</a>
+	<br>
+	
+	<form action="springmvc/testRestPost" method="post">
+		<input type="submit" value="testPost">
+	</form>
+	<br>
+	
+	<form action="springmvc/testRestPut/1" method="post">
+		<input type="hidden" name="_method" value="PUT"/>
+		<input type="submit" value="TestRest PUT"/>
+	</form>
+	<br>
+	
+	<form action="springmvc/testRestDelete/1" method="post">
+		<input type="hidden" name="_method" value="DELETE"/>
+		<input type="submit" value="TestRest DELETE"/>
+	</form>
+	<br>
 </body>
 </html>
--- SpringMVC-1/src/com/zs/springmvc/handlers/SpringMVCTest.java
+++ SpringMVC-1/src/com/zs/springmvc/handlers/SpringMVCTest.java
@@ -1,6 +1,7 @@
 package com.zs.springmvc.handlers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,10 +16,66 @@ public class SpringMVCTest {
 		System.out.println("testRequestMapping");
 		return SUCCESS;
 	}
-	
+	/**
+	 * 只允许post方法请求
+	 */
 	@RequestMapping(value = "/testMethod", method = RequestMethod.POST)
 	public String testMethod() {
 		System.out.println("testMethod");
+		return SUCCESS;
+	}
+	
+	/**
+	 * 指定url?后必须带上参数username,如果参数age=10是不合法的
+	 */
+	@RequestMapping(value="/testHeadersAndParameters",params={"username","age!=10"})
+	public String testHeadersAndParameters(){
+		System.out.println("testHeadersAndParameters");
+		return SUCCESS;
+	}
+	
+	/**
+	 *  *可以匹配任意字符
+	 */
+	@RequestMapping(value="/testAntPath/*/abc")
+	public String testAntPath(){
+		System.out.println("testAntPath");
+		return SUCCESS;
+	}
+	
+	/**
+	 * 读出url中的请求参数
+	 */
+	@RequestMapping(value="/testPathVariable/{id}")
+	public String testPathVariable(@PathVariable("id") Integer id){
+		System.out.println("testPathVariable  id:"+id);
+		return SUCCESS;
+	}
+	
+	/**
+	 * restfule api get方法
+	 */
+	@RequestMapping(value="/testRestGet/{id}",method=RequestMethod.GET)
+	public String testGet(@PathVariable("id") Integer id){
+		System.out.println("testGet  id:"+id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value="/testRestPost",method=RequestMethod.POST)
+	public String testPost(){
+		System.out.println("testRestPost");
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "/testRestPut/{id}", method = RequestMethod.PUT)
+	public String testRestPut(@PathVariable Integer id) {
+		System.out.println("testRest Put: " + id);
+		return SUCCESS;
+	}
+
+	@RequestMapping(value = "/testRestDelete/{id}", method = RequestMethod.DELETE)
+	public String testRestDelete(@PathVariable Integer id) {
+		System.out.println("testRest Delete: " + id);
 		return SUCCESS;
 	}
 }
