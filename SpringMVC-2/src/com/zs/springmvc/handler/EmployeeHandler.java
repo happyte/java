59a970d67bd06ca6ba8279c2f2164a9132b88476
happyte <1016917515@qq.com>
2017-07-13 14:08:03 +0800

    Spring MVC显示所有员工信息

--- SpringMVC-2/src/com/zs/springmvc/handler/EmployeeHandler.java
+++ SpringMVC-2/src/com/zs/springmvc/handler/EmployeeHandler.java
@@ -1,13 +1,22 @@
 package com.zs.springmvc.handler;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zs.springmvc.crud.dao.EmployeeDao;
 
 @Controller
 public class EmployeeHandler {
 	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
 	@RequestMapping("/emps")
-	public String list(){
+	public String list(Map<String,Object> map){
+		map.put("employees", employeeDao.getAll());
 		return "list";
 	}
 }
